65f334048845a8f44f29336b19810223854b69ce
TheOrangeInd <TheOrangeInd@DESKTOP-AAO5NUL>
2019-03-18 20:11:09 +0200

    lab2C#

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,66 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        MainClass program = new MainClass();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            program.circTest(textBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            program.coneTest(textBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            program.circTask(textBox1, (int)numericUpDown1.Value, checkBox1.Checked);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            program.coneTask(textBox1, (int)numericUpDown2.Value, checkBox2.Checked);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -8,15 +8,175 @@ namespace WindowsFormsApp1
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+    }
+
+    class MainClass
+    {
+        Random rand = new Random();
+
+        public void circTest(TextBox field)
+        {
+            Circle circ = new Circle((float)rand.Next(15) + 1);
+            field.Text = "Circle info:\r\n" + circ.info();
+        }
+
+        public void coneTest(TextBox field)
+        {
+            Cone cone = new Cone((float)rand.Next(15) + 1, (float)rand.Next(31) + 1);
+            field.Text = "Cone info:\r\n" + cone.info(true);
+        }
+
+        public void circTask(TextBox field, int N, bool detailed)
+        {
+            field.Text = "Сircles with area less than average:\r\n";
+
+            Circle[] arr = new Circle[N];
+            float sum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = new Circle((float)rand.Next(15) + 1);
+                sum += arr[i].getArea();
+            }
+
+            float averageArea = sum / N;
+            if (detailed) field.Text += "(Average area is " + averageArea + ")\r\n";
+            field.Text += "\r\n";
+
+            for (int i = 0; i < N; i++)
+            {
+                if(arr[i].getArea() < averageArea)
+                {
+                    field.Text += "CIRCLE " + (i + 1) + "\r\n";
+                    if (detailed) field.Text += arr[i].info() + "\r\n";
+                    field.Text += "\r\n";
+                }
+            }
+
+            field.Text += "\r\nFull list:\r\n\r\n";
+            for (int i = 0; i < N; i++)
+            {
+                field.Text += "CIRCLE " + (i + 1) + "\r\n" + arr[i].info() + "\r\n";
+            }
+        }
+
+        public void coneTask(TextBox field, int M, bool detailed)
+        {
+            field.Text = "Сone with the largest volume:\r\n";
+
+            Cone[] arr = new Cone[M];
+            float max = 0;
+            int result = 0;
+
+            for (int i = 0; i < M; i++)
+            {
+                arr[i] = new Cone((float)rand.Next(15) + 1, (float)rand.Next(31) + 1);
+                if (arr[i].getVolume() > max)
+                {
+                    max = arr[i].getVolume();
+                    result = i;
+                }
+            }
+
+            field.Text += "CONE " + (result + 1) + "\r\n";
+            if (detailed) field.Text += arr[result].info(true);
+            field.Text += "\r\n";
+
+            field.Text +=  "\r\nFull list:\r\n\r\n";
+            for(int i = 0; i < M; i++)
+            {
+                field.Text += "CONE " + (i + 1) + "\r\n" + arr[i].info(true) + "\r\n";
+            }
+        }
+    }
+
+    class Circle
+    {
+        public float R; //радиус окружности
+        public Circle(float radius)
+        {
+            this.R = radius;
+        }
+
+        //вычисление площади окружности
+        public float getArea()
+        {
+            return (float)(this.R * this.R * Math.PI);
+        }
+
+        //вычисление длины окружности
+        public float getLength()
+        {
+            return (float)(2 * this.R * Math.PI);
+        }
+
+        //вывод инф-ы об окружности
+        public void getInfo()
+        {
+            Console.WriteLine("Radius: " + R + "\r\nLength: " + getLength() + "\r\nArea: " + getArea() + "\r\n");
+        }
+
+        public string info()
+        {
+            return "Radius: " + R + "\r\nLength: " + getLength() + "\r\nArea: " + getArea() + "\r\n";
+        }
+    }
+
+    class Cone : Circle
+    {
+        float H; //высота конуса
+        public Cone(float radius, float height) : base(radius)
+        {
+            this.H = height;
+        }
+
+        //вычисление площади поверхности конуса
+        public new float getArea()
+        {
+            return (float)(base.getArea() + (R * Math.Sqrt(R * R + H * H) * Math.PI));
+        }
+
+        //вычисление площади поверхности конуса с помощью образующей
+        public float getArea(float coneGen)
+        {
+            return (float)(R * coneGen * Math.PI + base.getArea());
+        }
+
+        //вычисление объема
+        public float getVolume()
+        {
+            return (getArea() * this.H) / 3;
+        }
+
+        //вывод инф-ы о конусе (краткой)
+        public new void getInfo()
+        {
+            Console.WriteLine("Radius: " + R + "\r\nHeight: " + H + "\r\nVolume: " + getVolume() + "\r\n");
+        }
+
+        //вывод подробной инф-ы о конусе
+        public void getInfo(bool detailed)
+        {
+            getInfo();
+            if (detailed) Console.WriteLine("Surface area: " + getArea() + "\r\n");
+        }
+
+        public new string info()
+        {
+            return "Radius: " + R + "\r\nHeight: " + H + "\r\nVolume: " + getVolume() + "\r\n";
+        }
+
+        public string info(bool detailed)
+        {
+            if (!detailed) return info();
+            else return info() + "Surface area: " + getArea() + "\r\n";
         }
     }
 }
